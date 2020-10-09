eb18ce2fa255b436c2f24622b0671eb8c3921da6
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-09 20:57:46 +0100

    added some codewars solutions

--- AlgosAndData/src/com/LA/Katas/FavSolutions.java
+++ AlgosAndData/src/com/LA/Katas/FavSolutions.java
@@ -1,5 +1,14 @@
 package com.LA.Katas;
 
 public class FavSolutions {
+	public static int toBinary(int n) {
+    String s = "";
+    while (n > 0)
+    {
+        s =  ( (n % 2 ) == 0 ? "0" : "1") +s;
+        n = n / 2;
+    }
+    return Integer.parseInt(s);
+  }
 
 }
