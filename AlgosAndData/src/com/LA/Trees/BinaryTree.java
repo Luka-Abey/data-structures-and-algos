c4cd4b6046ea0718c24af8cea65e3509acc1612b
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-09 20:30:46 +0100

    starting BFS

--- AlgosAndData/src/com/LA/Trees/BinaryTree.java
+++ AlgosAndData/src/com/LA/Trees/BinaryTree.java
@@ -1,5 +1,15 @@
 package com.LA.Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
  TreeNode<E> root;
+ 
+ public void levelOrder() {
+	 Queue <TreeNode<E>> = new LinkedList <TreeNode<E>>();
+	 
+ }
 }
+
+
