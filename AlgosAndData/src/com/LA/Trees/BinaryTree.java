5c43be05b7370235d51a1821db5ade870b42acce
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-09 20:34:24 +0100

    BFS further

--- AlgosAndData/src/com/LA/Trees/BinaryTree.java
+++ AlgosAndData/src/com/LA/Trees/BinaryTree.java
@@ -3,12 +3,20 @@ package com.LA.Trees;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class BinaryTree {
+public class BinaryTree<E> {
  TreeNode<E> root;
  
  public void levelOrder() {
 	 Queue <TreeNode<E>> = new LinkedList <TreeNode<E>>();
-	 
+	 q.add(root);
+	 while(!q.isEmpty()) {
+		 TreeNode<E> curr = q.remove();
+		 if(curr != null) {
+			 curr.visit();
+			 q.add(curr.getLeftChild());
+			 q.add(curr.getRightChild());
+		 }
+	 }
  }
 }
 
