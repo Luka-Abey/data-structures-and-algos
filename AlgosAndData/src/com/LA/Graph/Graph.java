16bc3ac547a4333f691ecc7f63cda9f23673d60f
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-11 17:17:09 +0100

    moved onto graphs

---
+++ AlgosAndData/src/com/LA/Graph/Graph.java
@@ -0,0 +1,21 @@
+package com.LA.Graph;
+
+public abstract class Graph {
+	private int numVertices;
+	private int numEdges;
+	
+	public Graph() {
+		numVertices = 0;
+		numEdges = 0;
+	}
+	
+	public int getNumVertices() {
+		return numVertices;
+	}
+	
+	public int getNumEdges() {
+		return numEdges;
+	}
+	
+	
+}
